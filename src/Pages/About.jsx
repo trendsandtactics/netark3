409af2ed26edd38ceb7a4721b0ef2b506069b0d6
PENCIL-TT <hariharantt015@gmail.com>
2025-10-07 12:28:26 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,16 +4,16 @@ import Brand from "../Components/Brand/Brand";
 import BreadCumb from "../Components/Common/BreadCumb";
 import Project1 from "../Components/Project/Project1";
 
-const About = () => {
+export default function About() {
   return (
     <div className="about-page">
-      <BreadCumb Title="About Us"></BreadCumb>
+      <BreadCumb Title="About Us" />
 
       <About4
         MainImg="/assets/images/inner/about-us-thu.png"
         SubTitle="NETARK TECHNOLOGIES INDIA PVT. LTD."
         Title="About NETARK – Experts in Networking & Secure IT Infrastructure"
-        Content={`Founded by seasoned networking professionals with two decades of hands-on experience, NETARK Technologies India Pvt. Ltd. has grown into a trusted partner for businesses seeking robust and secure IT infrastructure solutions and Internet services.
+        Content={`Founded by seasoned networking professionals with two decades of hands-on experience, NETARK Technologies India Pvt. Ltd. has grown into a trusted partner for businesses seeking robust and secure IT infrastructure solutions and internet services.
 
 Our mission is simple: to empower businesses with IT systems that are secure, scalable, and built for performance.
 
@@ -23,24 +23,22 @@ We specialize in:
 • Enterprise network design & implementation
 • Server co-location and hosting services
 • VPS & Cloud Solutions
-• Cybersecurity & information security services
+• Cybersecurity & Information Security services
 • Unified communications & mobility platforms
 • Open-source IT solutions
 
 By working with global leaders like Cisco, HP, Lenovo, Sophos, FortiGate, Juniper, and Honeywell, we ensure our clients get world-class technology backed by local expertise.`}
         listTitle1="Secure & Scalable IT Infrastructure"
         listTitle2="Trusted IT & Internet Partner"
-      ></About4>
+      />
 
       <Project1
         bgImage="/assets/images/inner/project-bg-3.png"
         ClassAdd="project-area style-two"
-      ></Project1>
+      />
 
-      <Brand></Brand>
-      <Blog1></Blog1>
+      <Brand />
+      <Blog1 />
     </div>
   );
-};
-
-export default About;
+}
