cb1e2a27c03b7907784c89994b457cf702c0af67
PENCIL-TT <hariharantt015@gmail.com>
2025-10-08 20:55:52 +0530

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,13 +1,10 @@
 import BreadCumb from "../Components/Common/BreadCumb";
 import Contact1 from "../Components/Contact/Contact1";
-import Map from "../Components/Map/Map";
-
 const Contact = () => {
     return (
         <div className="contact-page">
             <BreadCumb Title="Contact"></BreadCumb>
             <Contact1></Contact1>
-            <Map></Map>
         </div>
     );
 };
