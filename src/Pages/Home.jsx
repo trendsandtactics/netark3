982986d383864d8508485427a2746c9540be4a26
PENCIL-TT <hariharantt015@gmail.com>
2025-10-08 18:31:47 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,7 @@
 import About1 from "../Components/About/About1";
 import Hero1 from "../Components/Banner/Hero1";
-import Blog1 from "../Components/Blog/Blog1";
-import Brand from "../Components/Brand/Brand";
 import Contact1 from "../Components/Contact/Contact1";
-import Faq from "../Components/Faq/Faq";
 import Features from "../Components/Features/Features";
-import Pricing from "../Components/Pricing/Pricing";
-import Project1 from "../Components/Project/Project1";
-import Services1 from "../Components/Services/Services1";
-import Testimonial1 from "../Components/Testimonial/Testimonial1";
 
 const Home = () => {
     return (
@@ -26,16 +19,7 @@ const Home = () => {
             <Features></Features>
             <About1></About1>
             <Services1></Services1>
-            <Project1 
-                bgImage="/assets/images/project-bg.png"
-                ClassAdd="project-area"
-            ></Project1>
-            <Brand></Brand>
-            <Pricing></Pricing>
-            <Faq></Faq>
-            <Testimonial1></Testimonial1>
             <Contact1></Contact1>
-            <Blog1></Blog1>
         </div>
     );
 };
