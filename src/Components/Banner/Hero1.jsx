8f63d43889f462560cce8e9a5a14ef1fe945d178
trendsandtactics <trendsntactics@gmail.com>
2025-10-09 10:40:17 +0530

    Update Hero1.jsx

--- src/Components/Banner/Hero1.jsx
+++ src/Components/Banner/Hero1.jsx
@@ -1,64 +1,74 @@
 import { useEffect, useState } from "react";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 import loadBackgroudImages from "../Common/loadBackgroudImages";
 import VideoModal from "../VideoModal/VideoModal";
 import { Link } from "react-router-dom";
 
-const Hero1 = ({bgImg,SubTitle,Title,Content,BtnText,BtnLink,Image,VideoText}) => {
+const Hero1 = ({ bgImg, SubTitle, Title, Content, BtnText, BtnLink, Image, VideoText }) => {
+  useEffect(() => {
+    loadBackgroudImages();
+  }, []);
 
-	useEffect(() => {
-        loadBackgroudImages();
-      }, []);
+  const [iframeSrc, setIframeSrc] = useState("about:blank");
+  const [toggle, setToggle] = useState(false);
 
-	  const [iframeSrc, setIframeSrc] = useState('about:blank');
-	  const [toggle, setToggle] = useState(false);
-	
-	  const handelClick = () => {
-		setIframeSrc("https://www.youtube.com/embed/rRid6GCJtgc");
-		setToggle(!toggle);
-	  };
-	  const handelClose = () => {
-		setIframeSrc('about:blank');
-		setToggle(!toggle);
-	  };
+  const handelClick = () => {
+    setIframeSrc("https://www.youtube.com/embed/rRid6GCJtgc");
+    setToggle(true);
+  };
+  const handelClose = () => {
+    setIframeSrc("about:blank");
+    setToggle(false);
+  };
 
-    return (
-        <div className="hero-area d-flex align-items-center" data-background={bgImg}>
-		<div className="container">
-			<div className="row hero align-items-center">
-				<div className="col-lg-6">
-					<div className="hero-contant">
-						<h5>{SubTitle}</h5>
-						<h1>{parse(Title)}</h1>
-						<p>{Content}</p>
-						<div className="solutek-btn">
-							<Link to={BtnLink}>
-								{BtnText}
-								<div className="solutek-hover-btn hover-bx"></div>
-								<div className="solutek-hover-btn hover-bx2"></div>
-								<div className="solutek-hover-btn hover-bx3"></div>
-								<div className="solutek-hover-btn hover-bx4"></div>
-							</Link>
-						</div>
-						<div className="hero-video-icon" onClick={handelClick}>	
-							<span className="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true"><i className="bi bi-play"></i><span>{VideoText}</span></span>
-						</div>
-					</div>
-				</div>
-				<div className="col-lg-6">
-					<div className="hero-thumb">
-						<img src={Image} alt="hero-thumb" />
-					</div>
-				</div>
-			</div>
-		</div>
-        <VideoModal
-          isTrue={toggle}
-          iframeSrc={iframeSrc}
-          handelClose={handelClose}        
-        ></VideoModal>
-	</div>
-    );
+  return (
+    <div className="hero-area d-flex align-items-center" data-background={bgImg}>
+      <div className="container">
+        <div className="row hero align-items-center">
+          <div className="col-lg-6">
+            <div className="hero-contant">
+              <h5>{SubTitle}</h5>
+              <h1>{parse(Title)}</h1>
+              <p>{Content}</p>
+
+              <div className="solutek-btn">
+                <Link to={BtnLink}>
+                  {BtnText}
+                  <div className="solutek-hover-btn hover-bx"></div>
+                  <div className="solutek-hover-btn hover-bx2"></div>
+                  <div className="solutek-hover-btn hover-bx3"></div>
+                  <div className="solutek-hover-btn hover-bx4"></div>
+                </Link>
+              </div>
+
+              {/* Video trigger WITHOUT the play icon */}
+              {VideoText && (
+                <button
+                  type="button"
+                  className="hero-video-icon video-vemo-icon venobox vbox-item"
+                  data-vbtype="youtube"
+                  data-autoplay="true"
+                  onClick={handelClick}
+                  aria-label="Play video"
+                  style={{ background: "transparent", border: "none", padding: 0, cursor: "pointer" }}
+                >
+                  <span>{VideoText}</span>
+                </button>
+              )}
+            </div>
+          </div>
+
+          <div className="col-lg-6">
+            <div className="hero-thumb">
+              <img src={Image} alt="hero-thumb" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <VideoModal isTrue={toggle} iframeSrc={iframeSrc} handelClose={handelClose} />
+    </div>
+  );
 };
 
 export default Hero1;
