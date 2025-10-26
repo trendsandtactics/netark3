e8f5ed1ac871e7d2600f844853d5d5d607790c44
PENCIL-TT <hariharantt015@gmail.com>
2025-10-26 11:47:13 +0530

    Create App.jsx

---
+++ src/Pages/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Solutions from "./pages/Solutions"; // ✅ Add this line
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/solutions" element={<Solutions />} /> {/* ✅ New route */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
